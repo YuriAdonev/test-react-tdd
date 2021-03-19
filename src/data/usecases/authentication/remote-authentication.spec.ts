f5563c3709710ee443088a95f7667f94e7decf5c
Yuri Adonev <zyztem@mail.ru>
2021-03-20 05:40:45 +0700

    refactor: change imports to use module

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,6 @@
-import { HttpPostClientSpy } from '../../test/mock-http-post-client'
+import { HttpPostClientSpy } from '@/data/test/mock-http-post-client'
 import { RemoteAuthentication } from './remote-authentication'
-import { mockAuthentication } from '../../../domain/test/mock-authentication'
+import { mockAuthentication } from '@/domain/test/mock-authentication'
 import faker from 'faker'
 
 type SutTypes = {
