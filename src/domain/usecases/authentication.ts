62265635a6384b7d64edb721fdf265abd1102937
Yuri Adonev <zyztem@mail.ru>
2021-03-20 02:44:14 +0700

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,10 @@
+import type { AccountModel } from '../models/account-model'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Autentication {
+  auth (params: AuthenticationParams): Promise<AccountModel>
+}
