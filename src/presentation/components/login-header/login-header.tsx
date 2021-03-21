311ff94bf1346aaa593c60338994ae8a7f6c4a72
Yuri Adonev <zyztem@mail.ru>
2021-03-22 00:08:35 +0700

    refactor: move header to a separate file

---
+++ src/presentation/components/login-header/login-header.tsx
@@ -0,0 +1,14 @@
+import React, { memo } from 'react'
+import Styles from './login-header-styles.scss'
+import Logo from '@/presentation/components/logo/logo'
+
+const LoginHeader: React.FC = () => {
+  return (
+    <header className={Styles.header}>
+      <Logo className={Styles.logo} />
+      <h1>Test project - react, typescript, TDD, clean architecture</h1>
+    </header>
+  )
+}
+
+export default memo(LoginHeader)
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import Spinner from '@/presentation/components/spinner/spinner'
 import Styles from './login-styles.scss'
-import Logo from '@/presentation/components/logo/logo'
+import Spinner from '@/presentation/components/spinner/spinner'
+import Header from '@/presentation/components/login-header/login-header'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo className={Styles.logo} />
-        <h1>Test project - react, typescript, TDD, clean architecture</h1>
-      </header>
+      <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
