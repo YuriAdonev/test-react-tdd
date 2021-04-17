e59d203520978b838b463d2bc08a331c7914f219
Yuri Adonev <zyztem@mail.ru>
2021-04-18 04:57:46 +0700

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,12 @@
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error = null
+  value: string
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    this.value = value
+    return this.error
+  }
+}
